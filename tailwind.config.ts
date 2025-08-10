bf9836363d233b4550efae9aba1dea69d78b4431
Lilian Fernandes <lilian.dias25@gmail.com>
2025-08-10 16:48:22 -0300

    fix(tailwind): remove unused tailwindcss-animate plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from 'tailwindcss'
 
 const config = {
   darkMode: 'class',
   content: ['./src/**/*.{ts,tsx}'],
-  plugins: [require('tailwindcss-animate')],
 } satisfies Config
 
 export default config
