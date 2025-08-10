046b759546516e8d2e00a8aad896df5fba383679
Lilian Fernandes <lilian.dias25@gmail.com>
2025-08-10 17:09:03 -0300

    feat: implement Card and Button components with styling and variants

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
 // TODO: Alterar fonte e metadados
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
 export const metadata: Metadata = {
   title: 'Baixada Vacinada',
@@ -25,12 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-amber-50 text-black antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt" className={inter.className}>
+      <body className="bg-amber-50 text-black antialiased">{children}</body>
     </html>
   )
 }
