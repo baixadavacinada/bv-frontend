8629c225b84a8d497d8d5720a2009b7482658c30
baixadavacinada <baixadavacinada@gmail.com>
2025-10-26 18:11:03 -0300

    feat: add  modal to agent user and admin user

--- src/components/design/BvUbsList.tsx
+++ src/components/design/BvUbsList.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { BvUbsCard, UbsCardProps } from '@/components/index'
 import { useAccessibilityValidation } from '@/hooks/use-accessibility'
 
 interface UbsListProps {
-  initialData: Omit<UbsCardProps, 'onMoreInfo' | 'onShare' | 'onFavoriteToggle'>[]
+  data: Omit<UbsCardProps, 'onMoreInfo' | 'onShare' | 'onFavoriteToggle' | 'onDelete'>[]
+  onDeleteRequest: (id: number) => void
+  onFavoriteToggleRequest: (id: number) => void
 }
 
-export function BvUbsList({ initialData }: UbsListProps) {
+export function BvUbsList({ data, onDeleteRequest, onFavoriteToggleRequest }: UbsListProps) {
   useAccessibilityValidation({ enabled: true })
-  const [ubsList, setUbsList] = useState(initialData)
   const router = useRouter()
 
   const handleMoreInfo = (id: number) => {
@@ -22,17 +22,12 @@ export function BvUbsList({ initialData }: UbsListProps) {
     alert(`Compartilhando: ${name}`)
   }
 
-  const handleFavoriteToggle = (id: number) => {
-    setUbsList((currentList) =>
-      currentList.map((ubs) => (ubs.id === id ? { ...ubs, isFavorite: !ubs.isFavorite } : ubs)),
-    )
-  }
-
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {ubsList.map((ubs) => (
+      {data.map((ubs) => (
         <BvUbsCard
           key={ubs.id}
+          id={ubs.id}
           name={ubs.name}
           slug={ubs.slug}
           neighborhood={ubs.neighborhood}
@@ -40,7 +35,8 @@ export function BvUbsList({ initialData }: UbsListProps) {
           isFavorite={ubs.isFavorite}
           onMoreInfo={() => handleMoreInfo(ubs.id || 0)}
           onShare={() => handleShare(ubs.name)}
-          onFavoriteToggle={() => handleFavoriteToggle(ubs.id || 0)}
+          onFavoriteToggle={() => onFavoriteToggleRequest(ubs.id || 0)}
+          onDelete={() => onDeleteRequest(ubs.id || 0)}
         />
       ))}
     </div>
