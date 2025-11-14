c7f6ca0ac09286dda1c679c3271efdc046b4c457
Baixada <baixadavacinada@gmail.com>
2025-11-14 13:27:32 -0300

    feat: updated criola's logo Co-authored-by: Paula <paulaisdev@gmail.com>

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -72,7 +72,7 @@ function LoginPageContent() {
             height: 'auto',
           }}
         />
-        <h1 className="mb-4 text-3xl font-bold text-gray-900">Baixada Vacinada</h1>
+        <h1 className="text-3xl font-bold text-gray-900">Baixada Vacinada</h1>
         <p className="text-lg text-gray-600">Bem-vindo(a) à nossa plataforma de vacinação!</p>
       </div>
 
