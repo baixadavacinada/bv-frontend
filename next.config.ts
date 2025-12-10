e3f123fc6b0810d23588d1dc3c2e1620c97c5680
Baixada <baixadavacinada@gmail.com>
2025-12-10 02:58:21 -0300

    refactor: fixed some warning issues Co-authored: Paula <paulaisdev@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-}
+const nextConfig: import('next').NextConfig = {}
 
 export default withNextIntl(nextConfig)
